77f2788b9ba337ac2641e0925e51a531aa9fc892
xuxiuyou <xuxiuyou@mathbeta.com>
2023-03-13 23:24:17 +0800

    optimize code

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,22 @@
 use clap::{App, Arg};
+use std::collections::HashMap;
 use std::io;
 use std::io::{Read, Write};
 use std::net::{Shutdown, TcpListener, TcpStream};
 use std::thread;
+use std::fs::File;
+use serde_yaml::Value;
+// use std::io::prelude::*;
 
 // get socket connection info
-fn stream_info(socket: &TcpStream) -> String {
-    let peer_addr = socket.peer_addr().unwrap().to_string();
-    let peer_addr = &peer_addr[..];
-    let local_addr = socket.local_addr().unwrap().to_string();
-    let local_addr = &local_addr[..];
-
-    format!("{} <=> {}", local_addr, peer_addr)
-}
+// fn stream_info(socket: &TcpStream) -> String {
+//     let peer_addr = socket.peer_addr().unwrap().to_string();
+//     let peer_addr = &peer_addr[..];
+//     let local_addr = socket.local_addr().unwrap().to_string();
+//     let local_addr = &local_addr[..];
+//
+//     format!("{} <=> {}", local_addr, peer_addr)
+// }
 
 // exchange data from sock1 to sock2, i.e. read data from sock1 and write it to sock2
 fn exchange(mut sock1: TcpStream, mut sock2: TcpStream) {
@@ -28,24 +32,24 @@ fn exchange(mut sock1: TcpStream, mut sock2: TcpStream) {
                 match v {
                     Ok(_) => {}
                     Err(e) => {
-                        eprintln!("Can not write data to {}: {}", stream_info(&sock2), e);
+                        eprintln!("Can not write data to {:?}: {}", sock2, e);
                         sock1
                             .shutdown(Shutdown::Both)
-                            .expect(&format!("Failed to shutdown {}", stream_info(&sock1))[..]);
+                            .expect(&format!("Failed to shutdown {:?}", sock1)[..]);
                         sock2
                             .shutdown(Shutdown::Both)
-                            .expect(&format!("Failed to shutdown {}", stream_info(&sock2))[..]);
+                            .expect(&format!("Failed to shutdown {:?}", sock2)[..]);
                     }
                 }
             }
             Err(e) => {
-                eprintln!("Can not read data from {}: {}", stream_info(&sock1), e);
+                eprintln!("Can not read data from {:?}: {}", sock1, e);
                 sock1
                     .shutdown(Shutdown::Both)
-                    .expect(&format!("Failed to shutdown {}", stream_info(&sock1))[..]);
+                    .expect(&format!("Failed to shutdown {:?}", sock1)[..]);
                 sock2
                     .shutdown(Shutdown::Both)
-                    .expect(&format!("Failed to shutdown {}", stream_info(&sock2))[..]);
+                    .expect(&format!("Failed to shutdown {:?}", sock2)[..]);
             }
         }
     }
@@ -91,12 +95,23 @@ fn proxy(proxy_addr: &str, target_addr: &str) {
     // handle each client in a new thread in case of blocking the listener
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        eprintln!("handling incoming connection: {}", stream_info(&stream));
+        eprintln!("handling incoming connection {:?}", stream);
         let sc = stream.try_clone().unwrap();
         let ta = target_addr.to_string();
 
         thread::spawn(|| do_proxy(sc, ta).unwrap_or_else(|error| eprintln!("{:?}", error)));
     }
+}
+
+// read configuration from yaml file
+fn read_config() -> HashMap<String, Value> {
+    let mut f=File::open("./config.yml").unwrap();
+    let mut buf=String::new();
+    f.read_to_string(&mut buf).unwrap();
+
+    let map: HashMap<String, Value> = serde_yaml::from_str(&buf).unwrap();
+    eprintln!("{:?}", map);
+    map
 }
 
 fn main() {
@@ -118,6 +133,8 @@ fn main() {
         eprintln!("Please set proxy address and target address:\n\tUsage: proxy <proxy address> <target address>\n\tExample: proxy 127.0.0.1:8080 127.0.0.1:80");
         std::process::exit(1);
     }
+
+    read_config();
 
     proxy(&proxy_addr, &target_addr);
 }
